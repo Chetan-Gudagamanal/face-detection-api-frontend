654248fda7ad107d23aed3d83bc21468e43e553c
Chetan-Gudagamanal <chetanbg95@gmail.com>
2023-10-21 13:57:40 +0530

    updated new backend url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const url = "https://face-detection-backend-server.vercel.app";
+export const url = "https://face-detection-backend-server-production.up.railway.app";
 // export const url='http://localhost:5000'
