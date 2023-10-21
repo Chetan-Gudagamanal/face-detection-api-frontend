e583bd5e1935015abedcdfde6af6a03491bc0694
Chetan-Gudagamanal <chetanbg95@gmail.com>
2023-10-21 12:13:37 +0530

    updated the backend url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const url='https://face-detection-backend-server.herokuapp.com'
+export const url = "https://face-detection-backend-server.vercel.app";
 // export const url='http://localhost:5000'
